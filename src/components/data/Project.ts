a3ef1f8c4a2e9768d7e6b8eeb208203c948247a9
Phornsydeth <phornsideth29@gmail.com>
2026-01-31 10:53:09 +0700

    I change link enter via data typescript

--- src/components/data/Project.ts
+++ src/components/data/Project.ts
@@ -13,7 +13,7 @@ export const projects: ProjectData[] = [
     title: "Secure User Authentication & Role-Based Access System",
     description: "This project is a banking-oriented backend authentication system developed using Spring Boot and Spring Security, designed to follow industry-level security standards used in financial applications. The system provides secure user registration and login functionality with JWT-based authentication, ensuring stateless and scalable session management. User credentials are safely stored using password encryption, preventing plaintext password exposure and aligning with security best practices commonly required in banking systems. A role-based access control (RBAC) mechanism is implemented to manage authorization between different user roles such as USER and ADMIN. Each role has controlled access to protected APIs, simulating real-world banking permission models. The application uses Spring Data JPA for efficient ORM-based database interaction and MySQL for persistent data storage. The architecture is designed to be modular, secure, and extensible—making it suitable as a foundation for larger banking or financial systems.",
     githubLink: "https://github.com/PhornSydeth/user_mangagment",
-    demoLink: "https://projectone.live",
+    demoLink: "https://sydethportfolio.netlify.app/#projects",
   },
   // Add more projects here
 ];
